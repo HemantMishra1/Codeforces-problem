85fb89567732b1504ac79a691844893d09c7cf47
hemant mishra <36245947+HemantMishra1@users.noreply.github.com>
2020-07-28 11:18:07 +0530

    Add files via upload

---
+++ 1339A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   
+   int n;
+   cin>>n;
+   int t;
+   while(n--){
+    
+    cin>>t;
+    cout<<t<<"\n";
+   }
+   
+   return 0;
+   
+  
+
+}
